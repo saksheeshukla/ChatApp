5b7257e6513769ada70721c5a9ff0a07bed29a4e
Sakshee Shukla <sakshee1234shukla@gmail.com>
2025-12-03 00:32:21 +0530

    defined all the routes

---
+++ src/routes/search.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const SearchController =  require('../controllers/search.controller');
+
+const router = express.Router();
+
+router.get('/search', SearchController.searchUsers);
+
+module.exports = router;
+
+
---
+++ src/routes/updateInfo.route.js
@@ -0,0 +1,8 @@
+const express  =  require('express');
+const usercontroller  =  require('../controllers/user.controllers.js');
+const router = express.Router();
+
+router.get('/updateavatar/:user/:id',usercontroller.updateavatar);
+
+module.exports = router;
+
